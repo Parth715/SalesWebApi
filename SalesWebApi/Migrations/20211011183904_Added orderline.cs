c0836fd8d04cce6f69e5982273637ecc90f4839f
Parth Patel <parth2002715@gmail.com>
2021-10-11 16:22:57 -0400

    Add project files.

---
+++ SalesWebApi/Migrations/20211011183904_Added orderline.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebApi.Migrations
+{
+    public partial class Addedorderline : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Orders",
+                newName: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Orderlines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Product = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orderlines", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orderlines_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orderlines_OrderId",
+                table: "Orderlines",
+                column: "OrderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orderlines");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Orders",
+                newName: "id");
+        }
+    }
+}
---
+++ SalesWebApi/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebApi.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Orderline> Orderlines { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder) { }
+    }
+}
